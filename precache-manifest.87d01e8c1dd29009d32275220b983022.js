b05e23966948074a1ea6b75ad084abb0dd89472a
createinnovateproduce <createinnovateproduce@gmail.com>
2019-08-25 14:20:17 -0500

    Updates

---
+++ precache-manifest.87d01e8c1dd29009d32275220b983022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847fa9f073a856383e748f598a8e7c2b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "806f535c830f60a707d3",
+    "url": "/monster-rolodex/static/css/main.f6e2b2fc.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monster-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "806f535c830f60a707d3",
+    "url": "/monster-rolodex/static/js/main.15e6f06a.chunk.js"
+  },
+  {
+    "revision": "b9d608cb1517ab0e40b5",
+    "url": "/monster-rolodex/static/js/runtime~main.8833da29.js"
+  }
+]);
